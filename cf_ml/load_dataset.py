9b1e1054098d3a93a79603da0909aca4c2e28fe2
ChengFR <459763742@qq.com>
2020-01-16 23:54:52 +0800

    add data interface

---
+++ cf_ml/load_dataset.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np 
+from sklearn import datasets
+import pandas as pd
+
+from data import Dataset
+
+def load_HELOC_dataset():
+    df = pd.read_csv('./data/FICO/heloc_dataset_v1.csv')
+    data = df.to_numpy()
+    x = data[:, 1:]
+    y = data[:, 0]
+    dd = pd.read_csv('./data/FICO/description.csv')
+    attr_disc = dd.to_dict('records')
+    return Dataset(attr_disc=attr_disc, name='HELOC', x=x, y=y)
+
+def load_german_credit_dataset():
+    pass
+
+def load_boston_housing_dataset():
+    pass
+
+
+if __name__ == '__main__':
+    dataset = load_HELOC_dataset()
