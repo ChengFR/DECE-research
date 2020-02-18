8a30870b1c3868f951d32af30877d51277dea9dd
ChengFR <459763742@qq.com>
2020-02-19 02:52:28 +0800

    rewrite dataset interface

--- cf_ml/load_dataset.py
+++ cf_ml/load_dataset.py
@@ -3,16 +3,24 @@ import numpy as np
 from sklearn import datasets
 import pandas as pd
 
-from data import Dataset
+# from data import Dataset
+from data_interface import Dataset
 
+# def load_HELOC_dataset():
+#     df = pd.read_csv('../data/HELOC/heloc_dataset_v2.csv')
+#     data = df.to_numpy()
+#     x = data[:, 1:]
+#     y = data[:, 0]
+#     dd = pd.read_csv('../data/HELOC/description.csv')
+#     attr_disc = dd.to_dict('records')
+#     return Dataset(attr_disc=attr_disc, name='HELOC', x=x, y=y)
 def load_HELOC_dataset():
-    df = pd.read_csv('../data/HELOC/heloc_dataset_v2.csv')
-    data = df.to_numpy()
-    x = data[:, 1:]
-    y = data[:, 0]
-    dd = pd.read_csv('../data/HELOC/description.csv')
-    attr_disc = dd.to_dict('records')
-    return Dataset(attr_disc=attr_disc, name='HELOC', x=x, y=y)
+    data_df = pd.read_csv('../data/HELOC/heloc_dataset_v1.csv')
+    description = pd.read_csv('../data/HELOC/description.csv', index_col='name').to_dict('index')
+    for _, info in description.items():
+        if type(info['category']) is str:
+            info['category'] = info['category'].split(' ')
+    return Dataset('HELOC', data_df, description, 'RiskPerformance')
 
 def load_german_credit_dataset():
     pass
