2ebf74c441bf5a1d9a6838795ddb13d60c5f009a
ChengFR <459763742@qq.com>
2020-02-19 23:22:33 +0800

    add pytorch implementation of a cf engine

---
+++ cf_ml/cf_engine/counterfactual.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import pandas as pd 
+
+
+class CounterfactualExample:
+    """A class to store counterfactual examples to an instance"""
+    def __init__(self):
+        pass
+
+class CounterfactualExampleBySubset:
+    """A class to store counterfactual examples to a subset"""
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    
