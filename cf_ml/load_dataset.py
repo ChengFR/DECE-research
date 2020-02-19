041e945cee0c611144aa50431ac7a321232780ea
ChengFR <459763742@qq.com>
2020-02-19 23:20:51 +0800

    update dataset loader

--- cf_ml/load_dataset.py
+++ cf_ml/load_dataset.py
@@ -3,17 +3,8 @@ import numpy as np
 from sklearn import datasets
 import pandas as pd
 
-# from data import Dataset
-from data_interface import Dataset
+from dataset import Dataset
 
-# def load_HELOC_dataset():
-#     df = pd.read_csv('../data/HELOC/heloc_dataset_v2.csv')
-#     data = df.to_numpy()
-#     x = data[:, 1:]
-#     y = data[:, 0]
-#     dd = pd.read_csv('../data/HELOC/description.csv')
-#     attr_disc = dd.to_dict('records')
-#     return Dataset(attr_disc=attr_disc, name='HELOC', x=x, y=y)
 def load_HELOC_dataset():
     data_df = pd.read_csv('../data/HELOC/heloc_dataset_v1.csv')
     description = pd.read_csv('../data/HELOC/description.csv', index_col='name').to_dict('index')
