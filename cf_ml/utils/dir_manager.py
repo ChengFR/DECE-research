c767da449f3f707013d3e7169102322a7b6ca088
ChengFR <459763742@qq.com>
2020-02-20 20:43:37 +0800

    change of name of the dirmanager to dir_manager

---
+++ cf_ml/utils/dir_manager.py
@@ -0,0 +1,26 @@
+import os
+
+output_root = './output/'
+
+class DirectoryManager:
+    """A class to manage the path of the model files"""
+    
+    def __init__(self):
+        pass
+
+    def load_from_meta(self, dirpath):
+        pass
+
+    def save_meta(self):
+        pass
+
+    def rmifexist(self, path):
+        pass
+
+    def alterifexist(self, path):
+        pass
+
+    def createifnotexist(self, dirpath):
+        pass
+
+
