82a0ee38b5e3f12c60bd2a32febeddd83964410a
ChengFR <459763742@qq.com>
2020-02-20 15:25:29 +0800

    add class counterfactualexample

--- cf_ml/cf_engine/counterfactual.py
+++ cf_ml/cf_engine/counterfactual.py
@@ -10,7 +10,21 @@ class CounterfactualExample:
 class CounterfactualExampleBySubset:
     """A class to store counterfactual examples to a subset"""
 
-    def __init__(self, dataset):
+    def __init__(self, dataset, cf_num):
         self.dataset = dataset
+        self.cf_num = cf_num
+        self.cf_df = pd.DataFrame()
+        self.instance_df = pd.DataFrame()
+
+    def append_cfs(self, cf_df, instance_df):
+        if len(self.cf_df) == 0:
+            self.cf_df = cf_df
+            self.instance_df = instance_df
+        else:
+            self.cf_df = self.cf_df.append(cf_df)
+            self.instance_df = self.instance_df.append(instance_df)
+
+    def _add_index_col(self):
+        self.cf_df['OriginIndex'] = np.array([(i//self.cf_num) for i in range(len(self.cf_df))])
 
     
