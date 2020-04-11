4129333500a61720eac745f6369c32ce4952844d
ChengFR <459763742@qq.com>
2020-04-11 23:53:55 +0800

    enable categorical feature modification

--- cf_ml/cf_engine/engine.py
+++ cf_ml/cf_engine/engine.py
@@ -229,6 +229,8 @@ class CFEnginePytorch:
                     self.cf_range[col]['max'] = info['max']
                 else:
                     self.cf_range[col]['category'] = info['category']
+
+        print(self.cf_range)
         self.normed_min = self.dataset.preprocess([info['min'] if self.desc[col]['type'] == 'numerical' else info['category'][0]
                                                    for col, info in self.cf_range.items()], mode='x')
         for col, info in self.desc.items():
@@ -246,6 +248,7 @@ class CFEnginePytorch:
                         self.normed_max['{}_{}'.format(col, cat)] = 1
                     else:
                         self.normed_max['{}_{}'.format(col, cat)] = 0
+        print(self.normed_max)
         self.normed_max = torch.from_numpy(self.normed_max.values).float()
 
     def init_cfs(self, data, mask):
