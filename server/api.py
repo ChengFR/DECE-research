a462bfdbe4a6b3b8552569203d9603b0a96cb890
ChengFR <459763742@qq.com>
2020-10-24 19:32:06 +0800

    update api/predict and api/cf_instance

--- server/api.py
+++ server/api.py
@@ -6,8 +6,6 @@ import json
 from flask.json import JSONEncoder
 from flask import request, jsonify, Blueprint, current_app, Response
 
-# from .cf_helpers import get_cf_by_index, get_cf_all
-# from .cf_helpers import data_meta_translate, group_cf
 from .helpers import trans_data_meta
 
 api = Blueprint('api', __name__)
@@ -57,19 +55,6 @@ class BetterJSONEncoder(JSONEncoder):
 # inject a more powerful jsonEncoder
 api.json_encoder = BetterJSONEncoder
 
-
-# def send_csv(df):
-#     output = io.StringIO()
-#     df.to_csv(output)
-#     return Response(output.getvalue(), mimetype="text/csv")
-
-
-# def get_data_dir(data_name, model_name):
-#     name = data_name if model_name is None else '{}/{}'.format(
-#         data_name, model_name)
-#     return os.path.join(current_app.config["DATA_DIR"], name)
-
-
 @api.route('/data_meta', methods=['GET'])
 def get_data_meta():
     data_meta = trans_data_meta(current_app.dir_manager.dataset_meta)
@@ -85,33 +70,7 @@ def get_data():
     data_df = current_app.dir_manager.load_prediction('dataset')
     return Response(data_df.to_csv(index=False), mimetype="text/csv")
 
-# will be soon deprecated
-@api.route('/cf', methods=['GET', 'POST'])
-def get_cf():
-    # changeable_attr = request.args.get('changeable_attr', default='all', type=str)
-    # cf_num = request.args.get('cf_num', default=1, type=int)
-    # data_range = {}
-    # desired_class = 'opposite'
-    # setting = {'changeable_attribute': changeable_attr, 'data_range': data_range, 'cf_num': cf_num, 'desired_class': desired_class}
-    setting = {'data_range': {}, 'cf_range': {}}
-    subset_cf = current_app.cf_engine.generate_cfs_from_setting(setting, batch_size=512)
-
-    index = None
-    if request.method == 'GET':
-        index = request.args.get('index', type=int)
-    elif request.method == 'POST':
-        index = request.json.get('index', None)
-    if index is not None:
-        cf_df = subset_cf.get_cf_by_origin_index(index)
-    else:
-        cf_df = subset_cf.get_cf()
-    data_meta = current_app.dir_manager.get_dataset_meta()
-    target_name = data_meta['target_name']
-    cols = current_app.dataset.get_columns(preprocess=False) + ['{}_pred'.format(target_name), 'OriginIndex']
-    cf_dict = group_cf(cf_df[cols])
-    return jsonify(cf_dict)
-
-@api.route('/r_counterfactuals', methods=['GET', 'POST'])
+@api.route('/r_counterfactuals', methods=['POST'])
 def get_cf_subset():
     request_params = request.get_json()
     filters = request_params["filters"]
@@ -125,73 +84,30 @@ def get_cf_subset():
 @api.route('/predict', methods=['POST'])
 def predict_instance():
     request_params = request.get_json()
-    query_instance_inlist = request_params['queryInstance']
-    features = current_app.dataset.get_feature_names(preprocess=False)
-    disc = current_app.dataset.get_description()
-    for i, name in enumerate(features):
-        if disc[name]['type'] == 'categorical':
-            if query_instance_inlist[i] not in disc[name]['category']:
-                query_instance_inlist[i] = int(query_instance_inlist[i])
-    query_instance = current_app.dataset.preprocess(query_instance_inlist, mode='x')
-    result = current_app.model.predict(query_instance_inlist)
-    print(result['{}_pred'.format(current_app.dataset.get_target_names(False))].values.tolist())
-    return str(result['{}_pred'.format(current_app.dataset.get_target_names(False))].values[0])
+    query_instance = request_params['queryInstance']
+    return current_app.model.report(x=query_instance)[current_app.dataset.prediction]
 
-@api.route('/cf_instance', methods=['GET', 'POST'])
+@api.route('/counterfactuals', methods=['GET', 'POST'])
 def get_cf_instance():
-    # print(request.get_json())
-    features = current_app.dataset.get_feature_names(preprocess=False)
     request_params = request.get_json()
-    query_instance_inlist = request_params['queryInstance']
-    target_label = request_params.get('target', 'opposite')
+    X = current_app.dataset.preprocess_X(request_params['queryInstance'])
     k = request_params.get('k', -1)
-    cf_num = request_params.get('cfNum', 1)
-    attr_flex = request_params.get('attrFlex', None)
-    disc = current_app.dataset.get_description()
-    if attr_flex:
-        changeable_attr = [features[i] for i in range(len(attr_flex)) if attr_flex[i]]
+    num = request_params.get('cfNum', 1)
+    attr_mask = request_params.get('attrFlex', None)
+    if attr_mask is not None:
+        changeable_attr = [current_app.dataset.features[i] for i, t in enumerate(attr_mask) if t]
     else:
         changeable_attr = 'all'
-    attr_range = {}
-    for filter in request_params['attrRange']:
-        name = filter["name"]
-        if name in features:
-            s = {}
-            if disc[name]['type'] == 'numerical':
-                _min = filter["extent"][0]
-                _max = filter["extent"][1]
-                # if _min > disc[name]["min"]:
-                s['min'] = _min
-                # if _max < disc[name]["max"]:
-                s['max'] = _max
-                attr_range[name] = s
-            else: 
-                # a tmp implementation
-                cats = []
-                for cat in filter['categories']:
-                    if cat not in disc[name]['category']:
-                        cats.append(int(cat))
-                    else:
-                        cats.append(cat)
-                s['category'] = cats
-                attr_range[name] = s
-    
-    for i, name in enumerate(current_app.dataset.get_feature_names(preprocess=False)):
-        if disc[name]['type'] == 'categorical':
-            if query_instance_inlist[i] not in disc[name]['category']:
-                query_instance_inlist[i] = int(query_instance_inlist[i])
+    range_list = request_params.get('attrRange', [])
+    for r in range_list:
+        if 'extent' in r:
+            r['min'], r['max']  = r['extent'][0], r['extent'][1]
+    cf_range = {r["name"]: {**r} for r in range_list}
 
-    setting = {'changeable_attribute': changeable_attr, 'cf_range': attr_range, 
-        'data_range': {}, 'cf_num': cf_num, 'desired_class': 'opposite', 'k': k}
-    print(query_instance_inlist, setting)
+    setting = {'changeable_attr': changeable_attr, 'cf_range': cf_range, 
+        'num': num, 'k': k}
+
+    print(X, setting)
     
-    subset_cf = current_app.cf_engine.generate_cfs_from_setting(setting, query_instance_inlist, 
-        diversity_weight=0.01, post_step=10)
-    cf_df = subset_cf.get_cf()
-    origin_df = subset_cf.get_instance()
-    pred_name = '{}_pred'.format(current_app.dataset.get_target_names(False))
-    pred_label = origin_df.loc[0, pred_name]
-    print(pred_label)
-    # cf_df = cf_df[cf_df[pred_name] != pred_label]
-    cols = current_app.dataset.get_feature_names(preprocess=False) + [pred_name]
-    return jsonify(cf_df[cols].values.tolist())
+    cfs = current_app.cf_engine.generate_cfs_from_setting(X, setting)
+    return jsonify(cfs.values.tolist())
